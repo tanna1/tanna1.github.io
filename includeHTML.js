e1849d9adbd59b484896c6b6ff070e99d4b9f8ff
thanna0 <kirakazakova111@gmail.com>
2022-03-13 19:41:11 +0300

    13.03

--- includeHTML.js
+++ includeHTML.js
@@ -1,7 +1,7 @@
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html'
+      var file = $(this).data('include')
       $(this).load(file)
     })
   })
