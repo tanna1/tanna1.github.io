0cb0710353e76796c19d787652a040904242d673
thanna0 <kirakazakova111@gmail.com>
2022-04-25 19:48:57 +0300

    travel time calculator
    changed page title
    added travel time calculator

--- ruler/ruler.js
+++ ruler/ruler.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
     var imageDpi = 1/13;
 
     var can = document.getElementById('canvas');
+    var distout = document.getElementById('distout');
+    var speedin = document.getElementById('speedin');
+    var timeout = document.getElementById('timeout');
     var ctx = can.getContext('2d');
     ctx.font = '15px serif';
-    var startX, startY;
+    var startX, startY, physicalLength;
 
     $("canvas").mousedown(function(event){
         startX = event.pageX;
@@ -13,9 +16,15 @@ $(document).ready(function(){
 
         $(this).bind('mousemove', function(e){
             drawLine(startX, startY, e.pageX, e.pageY);
+            distout.innerHTML = "Distance: " + Math.round(physicalLength) + " km / " + Math.round(physicalLength/1.609) + " miles";
+            setTime();
         });
     }).mouseup(function(){
         $(this).unbind('mousemove');
+    });
+
+    $("#speedin").on("input", function(){
+        setTime();
     });
 
     function drawLine(x, y, stopX, stopY){
@@ -29,15 +38,23 @@ $(document).ready(function(){
 
         // calculate length   
         var pixelLength = Math.sqrt(Math.pow((stopX - x),2) + Math.pow((stopY-y),2));
-        var physicalLength = pixelLength / imageDpi;
+        physicalLength = pixelLength / imageDpi;
 
         ctx.fillStyle = "#FFFFFF";
 
         var xPos = Math.min(stopX, 930);
         var yPos = Math.min(stopY, 700);
 
-
         ctx.fillText((Math.round(physicalLength) + " km"), xPos, yPos+25);
         ctx.fillText((Math.round(physicalLength/1.609) + " miles"), xPos, yPos+40);
+    }
+
+    function setTime(){
+        if (speedin.value != "" && speedin.value > 0){
+            var timeh = physicalLength/speedin.value;
+            timeout.innerHTML = "Time: " + Math.floor(timeh) + "h " + Math.round((timeh % 1)*60) + "m";
+        }
+        else
+            timeout.innerHTML = "Time: NaN";
     }
 });
